6ba2c72c8efa9ed62f6b1f25a2943f99b8be1343
xiaoyekanren <876670773@qq.com>
2019-12-05 16:48:42 +0800

    modify sys-keyfree_login.py,make it true

--- sys-keyfree_login.py
+++ sys-keyfree_login.py
@@ -2,6 +2,7 @@
 # Must already install [sshpass]
 import fabfile
 from fabric.api import *
+import os
 
 # 读取fabfile文件的cf参数
 cf = fabfile.cf
@@ -9,31 +10,43 @@ cf = fabfile.cf
 env.user = cf.get('keyfree_login', 'localuser')
 env.password = cf.get('keyfree_login', 'localuser_passwd')
 env.hosts = cf.get('keyfree_login', 'hosts').split(",")
-# 定义sudo用户参数
-sudouser = cf.get('keyfree_login', 'sudouser')
-sudouser_passwd = cf.get('keyfree_login', 'sudouser_passwd')
 # 需要拼接的字符串
 hostsum = len(env.hosts)
+
+
+# 卸载
+def uninstall():
+    run('rm -fr ~/.ssh')
 
 
 # 安装
 def install():
     run('rm -rf ~/.ssh/id_rsa*')  # 删除已有的公钥私钥
     run('ssh-keygen -t rsa -f ~/.ssh/id_rsa -P ""')  # 生成公钥新的公钥私钥
-    with settings(user=sudouser, password=sudouser_passwd):  # 安装sshpass
-        sudo('apt install sshpass -y')
-    if env.host == env.hosts[-1]:  # 即在最后一个主机运行的时候，此时全部主机已经生成了id_rsa，通过scp将全部公钥拿到最后这个主机上，生成authorized_keys文件，并scp传到其他全部主机上
-        i = 0
-        run('echo > ~/.ssh/authorized_keys')  # 创建或清空authorized_keys
-        while i < hostsum:  # 通过scp将全部公钥拿到最后这个主机上，全部存入authorized_keys文件，将传过来的文件删除
-            run('sshpass -p ' + env.password + ' scp ' + env.user + '@' + env.hosts[i] + ':~/.ssh/id_rsa.pub ./' + env.hosts[i] + ' && cat ' + env.hosts[i] + ' >> ~/.ssh/authorized_keys' + ' && rm -f ' + env.hosts[i])
-            i = i+1
-        i = 0
-        while i < hostsum:  # scp传到其他全部主机上
-            run('sshpass -p ' + env.password + ' scp ~/.ssh/authorized_keys ' + env.user + '@' + env.hosts[i] + ':~/.ssh/authorized_keys')
-            i = i+1
+
+    current_path = "".join(os.path.dirname(os.path.abspath('sys-keyfree_login.py')))  # 获取当前的路径
+    current_file_path = os.path.join(current_path, "".join(env.host)).replace('\\', '/')  # 写一个路径+文件名，为下一步骤get提供本地路径+文件名
+    get('.ssh/id_rsa.pub', current_file_path)  # 将公钥拿到本地
+
+    if env.host == env.hosts[-1]:  # 即在最后一个主机运行的时候，此时全部主机已经生成了id_rsa，然后执行install2
+        os.system('fab -f sys-keyfree_login.py install2')
 
 
-# 卸载
-def uninstall():
-    run('rm -fr ~/.ssh')
+def install2():
+    run('echo "' + addrsa() + '" > .ssh/authorized_keys')
+    if env.host == env.hosts[-1]:
+        os.system('fab -f sys-keyfree_login.py rmlocalfile')
+
+
+def addrsa():
+    rsa_global = ''
+    i = 0
+    while i < hostsum:
+        rsa = open(env.hosts[i]).read()
+        rsa_global = rsa_global + rsa
+        i += 1
+    return rsa_global
+
+
+def rmlocalfile():
+    os.remove(env.host)
