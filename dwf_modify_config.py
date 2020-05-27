87fee43ed86688e27ecdeec0d8bc05bfaffef7ee
xiaoyekanren <876670773@qq.com>
2020-05-27 14:31:03 +0800

    add dwf_modify_config.py's notes ,daily update

--- dwf_modify_config.py
+++ dwf_modify_config.py
@@ -29,14 +29,22 @@ def replace(filename, old_value, new_value):
     run("sed -i 's:" + bytes(old_value) + ":" + bytes(new_value) + ":g' " + filename)
 
 
+# # 以下都是需要修改的内容
+# # <!-------------------------
+# 普通用户账号密码
 env.user = 'ubuntu'
 env.password = 'Dwf12345'
+# sudo权限账号密码
 sudouser = 'ubuntu'
 sudouser_passwd = 'Dwf12345'
 
+# 这个是默认的三个端口
 default_host = {'front_page': 8180, 'model_manage': 6060, 'service_manage': 9090, 'object_manage': 7070}
+# 这个修改成第一台主机，以及映射出来的3个端口。默认IP递增1，step为有多少台主机
 the_first_host = {'first_host': '192.168.1.101', 'front_page': 8401, 'model_manage': 6281, 'service_manage': 9311, 'object_manage': 7291, 'step': 1}
+# 修改的2个配置文件
 replace_files = ['/opt/apache-tomcat-8.5.34/webapps/modeler-web/config.js', '/opt/apache-tomcat-8.5.34/webapps/app-web/config.js']
+# # --------------------------->
 
 env.hosts = addlist_hosts(first_host=the_first_host['first_host'], step=the_first_host['step'])
 port_front_page_list = addlist_port(first_port=the_first_host['front_page'], step=the_first_host['step'])
@@ -52,7 +60,3 @@ def modify():
             replace(filename=file, old_value=default_host['model_manage'], new_value=port_model_manage_list[index_place])
             replace(filename=file, old_value=default_host['service_manage'], new_value=port_service_manage_list[index_place])
             replace(filename=file, old_value=default_host['object_manage'], new_value=port_object_manage_list[index_place])
-
-
-
-
