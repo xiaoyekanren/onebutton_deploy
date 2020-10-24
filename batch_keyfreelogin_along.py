91bacd9b9bf18b2f138396b0e8ea40348f549fc1
xiaoyekanren <876670773@qq.com>
2020-10-24 19:15:37 +0800

    maven-add'user_home',batch-update'new version for keyfree'

--- batch_keyfreelogin_along.py
+++ batch_keyfreelogin_along.py
@@ -1,13 +1,19 @@
 # coding=UTF-8
-import configparser
-from fabric.api import *
-import os
+from configparser import ConfigParser
+from fabric.api import env, run, settings
+from os import system
 
+# python==2.7
 # fabfirc==1.14.1
-
+# pyinstaller=3.6
+# pypiwin32
+# pyinstaller -F batch_keyfreelogin_along.py
+# -F, –onefile	打包一个单个文件
+# 适用于新机器部署，会清理掉其他已存在的密钥
+# 完全体命令:python -m fabric -f batch_keyfreelogin_along.py install
 
 # 读取配置文件password.ini
-cf = configparser.ConfigParser()
+cf = ConfigParser()
 cf.read('config.ini')
 
 # 定义env
@@ -16,12 +22,14 @@ env.password = cf.get('keyfree_login', 'localuser_passwd')
 env.hosts = cf.get('keyfree_login', 'hosts').split(",")
 # 需要拼接的字符串
 hostsum = len(env.hosts)
+# 定义字典
+dict = {}
+alllist = env.hosts
 
 
 # 卸载
 def uninstall():
-    run('rm -rf ~/.ssh/id_rsa*')  # 删除已有的公钥私钥
-    # run('rm -rf ~/.ssh/authorized_keys')  # 删除当前的密钥
+    run('rm -rf ~/.ssh/*;')  # 删除已有的公钥私钥
 
 
 # 安装
@@ -29,31 +37,24 @@ def install():
     uninstall()
     run('ssh-keygen -t rsa -f ~/.ssh/id_rsa -P ""')  # 生成公钥新的公钥私钥
 
-    current_path = "".join(os.path.dirname(os.path.abspath('sys-keyfree_login.py')))  # 获取当前的路径
-    current_file_path = os.path.join(current_path, "".join(env.host)).replace('\\', '/')  # 写一个路径+文件名，为下一步骤get提供本地路径+文件名
-    get('.ssh/id_rsa.pub', current_file_path)  # 将公钥拿到本地
+    dict[env.host] = run('cat ~/.ssh/id_rsa.pub')  # 主机名:公钥  放入字典
 
     if env.host == env.hosts[-1]:  # 即在最后一个主机运行的时候，此时全部主机已经生成了id_rsa，然后执行install2
-        os.system('fab -f sys-keyfree_login.py install2')
+        # system('fab -f sys-keyfree_login.py install2')
+        install2()
+    # # ----------输出-----
+    # print ('finish')
+    # # 用来防止程序自动退出
+    # system('pause')
 
 
 def install2():
-    run('echo "' + addrsa() + '" > .ssh/authorized_keys')
-    if env.host == env.hosts[-1]:
-        os.system('fab -f sys-keyfree_login.py rmlocalfile')
-    run('chmod 600 ~/.ssh/authorized_keys')
-    run('chmod 700 ~/.ssh')
+    for x in env.hosts:  # 遍历env.hosts
+        with settings(host_string=x):  # 根据env.hosts在执行一遍
+            for y in dict.keys():  # 将字典写入authorized_keys
+                run('echo "' + dict[y] + '" >> .ssh/authorized_keys')
+            run('chmod 600 ~/.ssh/authorized_keys && chmod 700 ~/.ssh')  # 给文件夹权限
 
 
-def addrsa():
-    rsa_global = ''
-    i = 0
-    while i < hostsum:
-        rsa = open(env.hosts[i]).read()
-        rsa_global = rsa_global + rsa
-        i += 1
-    return rsa_global
-
-
-def rmlocalfile():
-    os.remove(env.host)
+if __name__ == '__main__':
+    install()
