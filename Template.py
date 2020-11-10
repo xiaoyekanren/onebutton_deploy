340f1c19ca040670b23276c793453c9d32d538b2
xiaoyekanren <876670773@qq.com>
2020-11-10 19:47:24 +0800

    update jdk,modify zookeeper and fabfile

--- Template.py
+++ Template.py
@@ -5,7 +5,8 @@ from fabric.api import *
 section = ''  # 指定config.ini的section名称
 cf = fabfile.cf  # 读取fabfile文件的cf参数
 # config.ini指定的通用参数
-env.hosts, env.user, env.password, sudouser, sudouser_passwd, local_file, software_folder, install_path = fabfile.get_common_var(section)
+env.hosts, env.user, env.password, sudouser, sudouser_passwd = fabfile.get_common_var(section)  # 取得主机列表、用户&密码、sudo用户&密码
+software_home = fabfile.get_software_home(section)  # 通过section或者软件home
 # config.ini指定的软件配置
 # a =
 # b =
@@ -17,12 +18,9 @@ env.hosts, env.user, env.password, sudouser, sudouser_passwd, local_file, softwa
 
 # 安装
 def install():
-    # 检查是否是root用户，是就退出
-    fabfile.check_user(env.user)
-    # 上传
-    upload_file = fabfile.upload(section)
-    # 解压
-    fabfile.decompress(section, upload_file)
+    fabfile.check_user(env.user)  # 检查是否是root用户，是就退出
+    upload_file = fabfile.upload(section)  # 上传
+    fabfile.decompress(section, upload_file, software_home, env.user, sudouser, sudouser_passwd)  # 解压,无返回值
     # 正式开始安装
     # 1
     # 2
