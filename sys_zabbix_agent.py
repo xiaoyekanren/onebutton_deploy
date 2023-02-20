6413b64873dbecfeb720cce14aa490321c77ffe8
xiaoyekanren <876670773@qq.com>
2023-02-20 13:24:55 +0800

    More suitable for general centos operating system

--- sys_zabbix_agent.py
+++ sys_zabbix_agent.py
@@ -10,25 +10,21 @@ env.user = cf.get(section, 'sudouser')
 env.password = cf.get(section, 'sudouser_passwd')
 sudouser = cf.get(section, 'sudouser')
 sudouser_passwd = cf.get(section, 'sudouser_passwd')
+# config
+package_url = cf.get(section,'package_url')
+zabbix_server = cf.get(section, 'zabbix_server')
+zabbix_package = package_url.split('/')[-1]
+zabbix_service = '-'.join(zabbix_package.split('-')[0:2])
 
 
-# 安装
 def install():
     with settings(warn_only=False):
-        run("sudo sed -e 's|^mirrorlist=|#mirrorlist=|g' \
-             -e 's|^#baseurl=http://mirror.centos.org|baseurl=https://mirrors.tuna.tsinghua.edu.cn|g' \
-             -i.bak \
-             /etc/yum.repos.d/CentOS-*.repo")
-        run('echo nameserver 166.111.8.28 > /etc/resolv.conf')
-        run('echo 101.6.15.130 mirrors.tuna.tsinghua.edu.cn >> /etc/hosts')
-        run('yum makecache')
-        run('yum install lsof net-tools vim wget pcre2 -y')
-        put('Tools/zabbix-agent2-6.2.4-release1.el7.x86_64.rpm', '/root/zabbix-agent2-6.2.4-release1.el7.x86_64.rpm')
-        # run('wget https://mirrors.tuna.tsinghua.edu.cn/zabbix/zabbix/6.2/rhel/7/x86_64/zabbix-agent2-6.2.4-release1.el7.x86_64.rpm --no-check-certificate')
-        run('rpm -ivh zabbix-agent2-6.2.4-release1.el7.x86_64.rpm')
-        run('sed -i "s/Server=127.0.0.1/Server=172.16.0.99/g" /etc/zabbix/zabbix_agent2.conf')
-        run('sed -i "s/# ListenPort=10050/ListenPort=10050/g" /etc/zabbix/zabbix_agent2.conf')
-        run('sed -i "s/ServerActive=127.0.0.1/ServerActive=172.16.0.99/g" /etc/zabbix/zabbix_agent2.conf')
-        run('sed -i "s/Hostname=Zabbix server/Hostname=`ifconfig -a | grep inet | grep -v inet6 | grep -v 127 | awk \'{print $2}\' | tr -d "addr:" | sed -n \'1p\'`/g" /etc/zabbix/zabbix_agent2.conf')
-        run('systemctl enable zabbix-agent2')
-        run('systemctl restart zabbix-agent2')
+        run('yum remove zabbix-agent zabbix-agent2')
+        run('wget %s' % package_url)
+        run('rpm -ivh %s' % zabbix_package)
+        run('sed -i "s/^Server=.*/Server=%s/g" /etc/zabbix/zabbix_agent2.conf' % zabbix_server)
+        run('sed -i "s/^ServerActive=.*/ServerActive=%s/g" /etc/zabbix/zabbix_agent2.conf' % zabbix_server)
+        run('sed -i "s/^Hostname=.*/Hostname=%s/g" /etc/zabbix/zabbix_agent2.conf' % env.host)
+        run('systemctl enable %s' % zabbix_service)
+        run('systemctl restart %s' % zabbix_service)
+        run('systemctl stop firewalld; systemctl disable firewalld')
