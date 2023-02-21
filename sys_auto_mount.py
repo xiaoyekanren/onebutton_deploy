73cee744120fed7f1985a4ba9123b8119eb7ec51
xiaoyekanren <876670773@qq.com>
2023-02-21 13:25:04 +0800

    change run to sudo

--- sys_auto_mount.py
+++ sys_auto_mount.py
@@ -19,27 +19,27 @@ mount_to_path = cf.get(section, 'mount_to_path').split(',')
 def install():
     # mkdir
     for i in mount_to_path:
-        run('mkdir -p %s' % i)
+        sudo('mkdir -p %s' % i)
     # parted && format
     for i in mount_dev:
         with settings(prompts={
             "Warning: The existing disk label on /dev/vdb will be destroyed and all data on this disk will be lost. Do you want to continue? \nYes/No? ": 'Yes',
             "Yes/No?": 'Yes'
         }):
-            run('parted %s mklabel gpt' % i)
-        run('parted %s mkpart primary 2048s 100%%' % i)
+            sudo('parted %s mklabel gpt' % i)
+        sudo('parted %s mkpart primary 2048s 100%%' % i)
     # get dev partition
     mount_dev_partition = []
     for i in mount_dev:
-        # mount_dev_partition.append(run('fdisk -l|grep %s|grep -v \'Disk\'| awk \'{print $1}\'' % i))
-        mount_dev_partition.append(i + run('parted /dev/sdb print | grep "Number  Start" -A1 | tail -n 1 | awk {\'print $1\'}'))
+        dev_partition = i + sudo('parted %s print | grep "Number  Start" -A1 | tail -n 1 | awk {\'print $1\'}' % i)
+        mount_dev_partition.append(dev_partition)
     # format
     for i in mount_dev_partition:
-        run('mkfs.ext4 %s' % i)
+        sudo('mkfs.ext4 %s' % i)
     # write to fstab
     for i in mount_dev_partition:
-        blkid = run('blkid %s | awk \'{print $2}\' | awk -F = \'{print $2}\' |sed \'s:"::g\'' % i)
-        run('echo UUID=%s %s ext4 defaults 0 0 >> /etc/fstab' % (str(blkid), str(mount_to_path[mount_dev_partition.index(i)])))
+        blkid = sudo('blkid %s | awk \'{print $2}\' | awk -F = \'{print $2}\' |sed \'s:"::g\'' % i)
+        sudo('echo UUID=%s %s ext4 defaults 0 0 >> /etc/fstab' % (str(blkid), str(mount_to_path[mount_dev_partition.index(i)])))
     # mount
     for i in mount_dev_partition:
-        run('mount %s %s' % (i, str(mount_to_path[mount_dev_partition.index(i)])))
+        sudo('mount %s %s' % (i, str(mount_to_path[mount_dev_partition.index(i)])))
